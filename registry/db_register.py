86011ea834aece45562f3e3230b57af628155739
yangyuexiong <yang6333yyx@126.com>
2022-12-02 15:28:59 +0800

    feat:register_redis改为新的MyAioRedis

--- registry/db_register.py
+++ registry/db_register.py
@@ -6,13 +6,10 @@
 # @Software: PyCharm
 
 
-import asyncio
 import aiomysql
 import aioredis
 
-from config.config import get_config
-
-conf = get_config()
+from common.libs.tools import MyAioRedis
 
 
 async def register_db(app):
@@ -29,7 +26,7 @@ async def register_db(app):
     await app['aio_mysql_engine'].wait_closed()
 
 
-async def register_redis(app):
+async def register_redis_v1(app):
     """
     aio redis pool注册
     :param app:
@@ -44,5 +41,25 @@ async def register_redis(app):
     app['aio_redis_engine'].close()
     await app['aio_redis_engine'].wait_closed()
 
-if __name__ == '__main__':
-    print(conf)
+
+async def register_redis(app):
+    """
+    aio redis pool注册
+    :param app:
+    :return:
+    """
+    aio_redis_conf = app.get('config').get('redis')
+    host = aio_redis_conf.get('redis_host')
+    db = int(aio_redis_conf.get('redis_db'))
+    password = aio_redis_conf.get('redis_pwd')
+    AIO_REDIS_CONF = {
+        "url": f"redis://{host}",
+        "password": password,
+        "db": db,
+        "encoding": "utf-8",
+        "decode_responses": True
+    }
+    app['aio_redis_engine'] = MyAioRedis(conf_dict=AIO_REDIS_CONF, is_pool=True).redis
+    yield
+    app['aio_redis_engine'].close()
+    await app['aio_redis_engine'].wait_closed()
